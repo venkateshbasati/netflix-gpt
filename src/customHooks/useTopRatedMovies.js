a0913a0b3da7354bc2eb24902a7f9274f3ba5a6f
Venkatesh Basati <137296245+venkateshbasati@users.noreply.github.com>
2023-09-18 20:28:29 +0530

    Update useTopRatedMovies.js

--- src/customHooks/useTopRatedMovies.js
+++ src/customHooks/useTopRatedMovies.js
@@ -8,7 +8,7 @@ const dispatch=useDispatch()
 const getTopRatedMovies=async()=>{
     const data=await fetch('https://api.themoviedb.org/3/movie/top_rated?page=1', {
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NDMxNGJiYzZmN2RlODE0ZTM5OTJiMGY0OTlkMWIxZSIsInN1YiI6IjY1MDQyMTI5ZmZjOWRlMGVlMjBhZDk2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.suVD4hUSMxTJpAs8Ch8QeHMhg8KqkIW6ZT4-SgsBCNI',
+            'Authorization': 'Bearer'+process.env.REACT_APP_TMDB_KEY,
             'accept': 'application/json'
         }
     });
